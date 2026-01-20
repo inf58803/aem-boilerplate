7d9ef9c851b8c45ded16ab25679eb65c06876b18
inf58803 <inf58803@adobe.com>
2026-01-20 13:36:20 +0530

    added placeholders

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,7 +1,10 @@
-export default function decorate(block) {
-  const [quoteWrapper] = block.children;
-
-  const blockquote = document.createElement('blockquote');
-  blockquote.textContent = quoteWrapper.textContent.trim();
-  quoteWrapper.replaceChildren(blockquote);
+import { fetchPlaceholders } from "../../scripts/aem";
+export default async function decorate(block) {
+    const placeholders = await fetchPlaceholders();
+    if(placeholders.quote_suffix) {
+        const suffix = document.createElement('div');
+        suffix.className='quote_suffix';
+        suffix.textContent= placeholders.quote_suffix;
+        block.appendChild(suffix);
+    }   
 }
